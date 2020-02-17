e67017df98f9e06656d851a5590bc3ec512b5a8e
Rahime Tayfun <suheyladurna@gmail.com>
2020-02-17 17:39:00 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./App2.css";
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      isOpen:false
+    }
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="main-header-container">
+        <nav>
+          <div className="nav-container">
+            <a className="title-link">Start Bootstrap</a>
+            <ul className="links-container">
+              <li>SERVICES</li>
+              <li>PORTFOLIO</li>
+              <li>ABOUT</li>
+              <li>TEAM</li>
+              <li>CONTACT</li>
+            </ul>
+
+            <div className="menu-toggle" onClick={()=>{this.setState({isOpen:!this.state.isOpen})}}>
+            <p>MENU</p>
+            <img width="12px" height="10px" src="https://i.ya-webdesign.com/images/hamburger-menu-icon-png-white-6.png"/>
+            </div>
+          </div>
+        </nav>
       </header>
+        <menu className={this.state.isOpen? "menu-open": ""}>
+          <p>SERVICES</p>
+          <p>PORTFOLIO</p>
+          <p>ABOUT</p>
+          <p>TEAM</p>
+          <p>CONTACT</p>
+        </menu>
+        <div className="text-container">
+          <div className="welcome-text-one">Welcome To Our Studio!</div>
+          <div className="welcome-text-two">IT'S NICE TO MEET YOU</div>
+          <a className="tell-more-button">TELL ME MORE</a>
+        </div>
+      <section className="section-container">
+
+      </section>
     </div>
   );
+}
 }
 
 export default App;
